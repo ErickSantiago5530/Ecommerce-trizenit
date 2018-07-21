95e0186cf66b91717eb58f83a5646177fb8b026f
Erick Santiago <santossantiagoerick@gmail.com>
2018-07-21 01:52:35 -0500

    prueba componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuHeaderComponent } from './menu-header/menu-header.component';
+import { MenuFooterComponent } from './menu-footer/menu-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuHeaderComponent,
+    MenuFooterComponent,
   ],
   imports: [
     BrowserModule
